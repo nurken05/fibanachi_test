b8a582cb187c75a65486a5eff7f7da9575621bb1
nurken05 <nurken.tenlesov2005@gmail.com>
2023-06-07 14:13:56 +0600

    v2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 using namespace std;
 int fibonacci(int n) {
-    if (n <= 1)
-        return n;
-    else
-        return fibonacci(n - 1) + fibonacci(n - 2);
+    //another implementation
 }
 
 int main() {
